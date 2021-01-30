eda3431c0c2549429af4d6d87a86397480f11455
Ethan Swan <eswan18@rocketmail.com>
2021-01-29 19:22:22 -0600

    Rename creds to S3_ instead of AWS_.

--- storage.py
+++ storage.py
@@ -8,10 +8,10 @@ conf = config['DEFAULT']
 
 s3 = boto3.resource(
         service_name='s3',
-        aws_access_key_id=conf['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=conf['AWS_SECRET_ACCESS_KEY']
+        aws_access_key_id=conf['S3_ACCESS_KEY_ID'],
+        aws_secret_access_key=conf['S3_SECRET_ACCESS_KEY']
 )
 
 def write_string_to_object(object_name, string):
-    bucket = s3.Bucket(conf['AWS_BUCKET'])
+    bucket = s3.Bucket(conf['S3_BUCKET'])
     bucket.put_object(Key=object_name, Body=string)
