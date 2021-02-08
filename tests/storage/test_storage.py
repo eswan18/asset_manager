c202d900d9bdf9a042788fe0cf84c4e9355e9a65
Ethan Swan <eswan18@rocketmail.com>
2021-02-07 22:01:39 -0600

    Update tests to cover strings and bytes.

--- tests/storage/test_storage.py
+++ tests/storage/test_storage.py
@@ -8,10 +8,14 @@ from asset_manager import storage
 
 s3_name_regex = r'[a-z0-9]([a-z0-9]|[-.]){1,61}[a-z0-9]'
 
-@given(obj=st.from_regex(s3_name_regex), text=st.text())
+@given(
+    obj=st.from_regex(s3_name_regex),
+    text=st.one_of(st.text(), st.binary())
+)
 def test_write_string_to_object(text, obj):
     mock_bucket = MagicMock(spec=['put_object'])
     with patch.object(storage._s3, 'Bucket', return_value=mock_bucket) as mock_method:
         storage.write_string_to_object(object_name=obj, text=text)
         storage._s3.Bucket.assert_called_with(storage.conf['S3_BUCKET'])
-        mock_bucket.put_object.assert_called_with(Key=obj, Body=text.encode())
+        text_as_bytes = text.encode() if isinstance(text, str) else text
+        mock_bucket.put_object.assert_called_with(Key=obj, Body=text_as_bytes)
