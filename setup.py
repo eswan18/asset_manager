9e13c2fbddf7f5209b0228c983c49b4b50b0eb03
Ethan Swan <eswan18@rocketmail.com>
2021-02-07 19:28:32 -0600

    Manage dependencies better.

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from distutils.core import setup
 
+requires = [
+    'altair',
+    'pandas',
+    'boto3',
+    'google-api-core',
+    'google-auth-oathlib',
+    'googleapis-common-props'
+]
+
+tests_require = requires + [
+    'hypothesis',
+    'pytest',
+    'pytest-cov',
+    'mypy'
+]
+
 setup(
     name='asset_manager',
     version='0.0.9',
     packages=['asset_manager'],
+    install_requires=requires,
+    extras_require={'tests': tests_require},
     license='MIT',
     long_description=open('README.md').read(),
 )
