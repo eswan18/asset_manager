3d60047278294d358d75fd8b2233bbd5092548e8
Ethan Swan <eswan18@rocketmail.com>
2021-02-07 22:19:46 -0600

    Clean up tests a little.

--- tests/storage/test_storage.py
+++ tests/storage/test_storage.py
@@ -1,21 +1,22 @@
 from unittest.mock import Mock, MagicMock, patch, create_autospec
+from typing import Union
 
 from hypothesis import given, strategies as st
 
 from asset_manager import storage
 
-# from mocks import MockBucket
 
-s3_name_regex = r'[a-z0-9]([a-z0-9]|[-.]){1,61}[a-z0-9]'
+S3_NAME_REGEX = r'[a-z0-9]([a-z0-9]|[-.]){1,61}[a-z0-9]'
+
 
 @given(
-    obj=st.from_regex(s3_name_regex),
+    obj_name=st.from_regex(S3_NAME_REGEX),
     text=st.one_of(st.text(), st.binary())
 )
-def test_write_string_to_object(text, obj):
+def test_write_string_to_object(obj_name: str, text: Union[str, bytes]):
     mock_bucket = MagicMock(spec=['put_object'])
     with patch.object(storage._s3, 'Bucket', return_value=mock_bucket) as mock_method:
-        storage.write_string_to_object(object_name=obj, text=text)
+        storage.write_string_to_object(object_name=obj_name, text=text)
         storage._s3.Bucket.assert_called_with(storage.conf['S3_BUCKET'])
         text_as_bytes = text.encode() if isinstance(text, str) else text
-        mock_bucket.put_object.assert_called_with(Key=obj, Body=text_as_bytes)
+        mock_bucket.put_object.assert_called_with(Key=obj_name, Body=text_as_bytes)
