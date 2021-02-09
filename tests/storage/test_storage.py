922256fb208ec1ab588ca59584c9e450723c83b1
Ethan Swan <eswan18@rocketmail.com>
2021-02-08 21:43:16 -0600

    Add test of s3 service builder.

--- tests/storage/test_storage.py
+++ tests/storage/test_storage.py
@@ -11,6 +11,28 @@ S3_NAME_REGEX = r'[a-z0-9]([a-z0-9]|[-.]){1,61}[a-z0-9]'
 
 
 @given(
+    key_id=st.text(max_size=50),
+    access_key=st.text(max_size=150)
+)
+def test_s3_service_builder(key_id, access_key):
+    secret = {'S3_ACCESS_KEY_ID': key_id,
+              'S3_SECRET_ACCESS_KEY': access_key}
+    with (
+        patch.object(storage, 'get_secret', return_value=secret),
+        patch.object(storage.boto3, 'resource', return_value='result')
+    ):
+        # Relevant function call.
+        s3 = storage._s3()
+        # Assertions.
+        storage.boto3.resource.assert_called_with(
+            service_name='s3',
+            aws_access_key_id=key_id,
+            aws_secret_access_key=access_key
+        )
+        assert s3 == 'result'
+
+
+@given(
     obj_name=st.from_regex(S3_NAME_REGEX),
     text=st.one_of(st.text(), st.binary())
 )
@@ -21,8 +43,9 @@ def test_write_string_to_object(obj_name: str, text: Union[str, bytes]):
     mock_s3_service.Bucket.return_value = mock_bucket
     # Patch them in.
     with patch.object(storage, '_s3', return_value=mock_s3_service):
+        # Relevant function call.
         storage.write_string_to_object(object_name=obj_name, text=text)
-
+        # Assertions.
         mock_s3_service.Bucket.assert_called_with(storage.conf['S3_BUCKET'])
         text_as_bytes = text.encode() if isinstance(text, str) else text
         mock_bucket.put_object.assert_called_with(Key=obj_name, Body=text_as_bytes)
