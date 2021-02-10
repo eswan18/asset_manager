4d21ebc1d600ec7363561fbee2d3e71603385f23
Ethan Swan <eswan18@rocketmail.com>
2021-02-09 20:27:17 -0600

    Update lambda entrypoint to return chart.

--- lambda_entrypoint.py
+++ lambda_entrypoint.py
@@ -1,7 +1,8 @@
 from asset_manager.dashboard import make_charts
 
 def lambda_handler(event, context):
+    html = make_charts().to_html()
     return {
       'statusCode': 200,
-      'body': 'HELLO'
+      'body': html
     }
