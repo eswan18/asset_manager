4d1ae2308a4c2d815d8b1d42e5e6a7abe8e876a4
Ethan Swan <eswan18@rocketmail.com>
2021-02-10 20:12:01 -0600

    Add content-type header to lambda entrypoint.

--- lambda_entrypoint.py
+++ lambda_entrypoint.py
@@ -3,6 +3,9 @@ from asset_manager.dashboard import make_charts
 def lambda_handler(event, context):
     html = make_charts().to_html()
     return {
-      'statusCode': 200,
-      'body': html
+        'statusCode': 200,
+        'body': html,
+        'headers': {
+            'Content-Type': 'text/html',
+        }
     }
