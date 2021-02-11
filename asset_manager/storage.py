7de7000f1202b6dc4281de4afe0de6c676e7770e
Ethan Swan <eswan18@rocketmail.com>
2021-02-10 20:51:39 -0600

    Add type hints for boto3 classes.

--- asset_manager/storage.py
+++ asset_manager/storage.py
@@ -2,10 +2,17 @@ from configparser import ConfigParser
 from io import BytesIO
 import base64
 import json
-from typing import Any, Union, List, Dict, Literal, overload
+from typing import (
+    Any, Optional, Union, List, Dict, Literal, overload, TYPE_CHECKING
+)
 import pkg_resources
 
 import boto3
+
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3.service_resource import S3ServiceResource, Bucket
+BucketLikeType = Union[str, Bucket]
 
 
 config_contents = pkg_resources.resource_string(__name__, "data/config.ini")
@@ -14,25 +21,62 @@ config.read_string(config_contents.decode())
 conf = config['DEFAULT']
 
 
-def _s3() -> Any:
+def _s3() -> 'S3ServiceResource':
     s3 = boto3.resource(service_name='s3')
     return s3
 
 
+def _default_bucket() -> Bucket:
+    return _s3().Bucket(conf['S3_BUCKET'])
+
+
+def _bucket_from_argument(
+    bucket: Optional[BucketLikeType]
+) -> Bucket:
+    if bucket is None:
+        return _default_bucket()
+    if isinstance(bucket, str):
+        return _s3().Bucket(bucket)
+    else:  # Assume it's a bucket object already.
+        return bucket
+
+
 def write_string_to_object(
     object_name: str,
-    text: Union[str, bytes]
+    text: Union[str, bytes],
+    bucket: Optional[BucketLikeType] = None
 ) -> None:
+    '''
+    Write a string into an object in S3.
+    '''
+    bucket = _bucket_from_argument(bucket)
     if isinstance(text, str):
         text = text.encode()
     bucket = _s3().Bucket(conf['S3_BUCKET'])
+    _default_bucket()
     bucket.put_object(Key=object_name, Body=text)
 
 
 def read_string_from_object(
-    object_name: str
+    object_name: str,
+    bucket: Optional[BucketLikeType] = None
 ) -> str:
-    bucket = _s3().Bucket(conf['S3_BUCKET'])
+    '''
+    sdf
+
+    Parameters
+    ----------
+    object_name:
+        Name of the object in the bucket.
+    bucket:
+        An S3 bucket object. Defaults to the bucket in the configuration file.
+
+    Returns
+    -------
+    string:
+        The string read from the object.
+    '''
+    bucket = _bucket_from_argument(bucket)
     b = BytesIO()
     bucket.download_fileobj(Key=object_name, Fileobj=b)
     # Must return to the beginning of the file before reading.
@@ -42,8 +86,18 @@ def read_string_from_object(
     return result_str
 
 
-def list_objects_in_bucket() -> List[str]:
-    bucket = _s3().Bucket(conf['S3_BUCKET'])
+def list_objects_in_bucket(
+    bucket: Any = None
+) -> List[str]:
+    '''
+    Get the names of all objects in a bucket.
+
+    Parameters
+    ----------
+    bucket:
+        An S3 bucket object. Defaults to the bucket in the configuration file.
+    '''
+    bucket = _bucket_from_argument(bucket)
     objects = [o.key for o in bucket.objects.all()]
     return objects
 
