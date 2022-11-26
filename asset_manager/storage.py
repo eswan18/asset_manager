a01be0ebc4a2ef416a6b84035b36cf7209b42cb8
Ethan Swan <ethanpswan@gmail.com>
2022-11-25 19:32:52 -0600

    Remove completely unused function.

--- asset_manager/storage.py
+++ asset_manager/storage.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 from configparser import ConfigParser
 from io import BytesIO
-import base64
-import json
-from typing import Any, Optional, Union, List, Dict, Literal, overload, TYPE_CHECKING
+from typing import Any, Optional, Union, List, TYPE_CHECKING
 import pkg_resources
 
 import boto3
@@ -104,51 +102,3 @@ def list_objects_in_bucket(bucket: Any = None) -> List[str]:
     except NoCredentialsError as exc:
         raise Exception("No s3 credentials, you may need to sign in") from exc
     return objects
-
-
-@overload
-def get_secret(secret_name: str, assume_json: Literal[True] = True) -> Dict[str, str]:
-    ...
-
-
-@overload
-def get_secret(secret_name: str, assume_json: Literal[False]) -> Union[str, bytes]:
-    ...
-
-
-def get_secret(secret_name: str, assume_json: bool = True):
-    """
-    Fetch a secret from AWS Secrets Manager.
-
-    Optionally convert it to a JSON object
-
-    Parameters
-    ----------
-    secret_name
-        The name of the secret
-    assume_json
-        Whether to treat the secret as a simple JSON dictionary of {str: str} and return
-        a decoded object from it.
-
-    Returns
-    -------
-    secret
-        The fetched secret
-    """
-    secret_name = "asset-manager-s3"
-    region_name = "us-east-2"
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html  # noqa: E501
-    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-    # Decrypt secret using the associated KMS CMK. Depending on whether the secret is
-    # a string or binary, one of these fields will be populated.
-    if "SecretString" in get_secret_value_response:
-        secret = get_secret_value_response["SecretString"]
-    else:
-        secret = base64.b64decode(get_secret_value_response["SecretBinary"])
-    if assume_json:
-        return json.loads(secret)
-    else:
-        return secret
