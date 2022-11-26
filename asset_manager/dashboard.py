a1e886bab643d6ffc31ca261239ddbafc0acdfcf
Ethan Swan <ethanpswan@gmail.com>
2022-11-26 14:30:52 -0600

    Create datastore submodule.

--- asset_manager/dashboard.py
+++ asset_manager/dashboard.py
@@ -1,60 +1,11 @@
-import re
-from io import StringIO
-
 import pandas as pd
 import altair as alt
 
-from .s3 import read_string_from_object, list_objects_in_bucket
-
-
-DAILY_SUMMARY_NAME_REGEX = re.compile(r"summaries_(\d{4}_\d{2}_\d{2}).csv")
-
-
-def add_date_column_from_name(df: pd.DataFrame, name: str) -> pd.DataFrame:
-    match = DAILY_SUMMARY_NAME_REGEX.match(name)
-    if match is None:
-        raise ValueError(f"name {name} does not resemble a daily file.")
-    date = match.groups()[0]
-    df2 = df.copy()
-    df2["Date"] = date.replace("_", "-")
-    return df2
-
-
-def extract_df_from_object(name: str) -> pd.DataFrame | None:
-    """
-    Given an object name, extract the DataFrame stored in it.
-
-    If the object name doesn't match our expected convention, None will be
-    returned.
-    """
-    string_io = StringIO()
-    string_io.write(read_string_from_object(name))
-    string_io.seek(0)
-    df = pd.read_csv(string_io)
-    string_io.close()
-    return df
-
-
-def get_daily_data() -> pd.DataFrame:
-    # Get all objects and limit down to the names that look like daily data.
-    object_names = list_objects_in_bucket()
-    daily_object_names = (name for name in object_names if DAILY_SUMMARY_NAME_REGEX.match(name)) 
-    # Pull out a DataFrame in each object and add a date column.
-    dfs = (extract_df_from_object(name) for name in daily_object_names)
-    dfs = (add_date_column_from_name(df, name) for df, name in zip(dfs, daily_object_names))
-    # Merge them all into one, since the new Date column will make them distinct.
-    full_df = pd.concat(dfs)
-    # Some of the data may have had an dummy column from its original index values.
-    bad_col = "Unnamed: 0"
-    if bad_col in full_df.columns:
-        full_df = full_df.drop(bad_col, axis=1)
-    # Convert the date column into a Pandas date.
-    full_df["Date"] = pd.to_datetime(full_df["Date"])
-    return full_df
+from .datastore import get_all_data
 
 
 def make_charts() -> alt.Chart:
-    data = get_daily_data()
+    data = get_all_data()
     asset_data = data[data.Type == "asset"]
     liability_data = data[data.Type == "liability"]
     # Prepare a dataset of net assets by day.
---
+++ asset_manager/datastore.py
@@ -0,0 +1,96 @@
+"""
+A wrapper over writing and reading data.
+
+The current underlying datastore is s3 but that could change, and the interface for this
+module should be unaffected.
+"""
+import re
+from io import StringIO
+from typing import Iterator
+
+import pandas as pd
+
+from .s3 import list_objects_in_bucket, read_string_from_object
+
+# Data is stored in daily files until the end of the year, at which point it's
+# (interactively) consolidated into a "yearly" file.
+# Yearly data has a Date column but daily data doesn't; the date must be inferred from
+# the object name.
+DAILY_SUMMARY_NAME_REGEX = re.compile(r"summaries_(\d{4}_\d{2}_\d{2}).csv")
+YEARLY_SUMMARY_NAME_REGEX = re.compile(r"summaries_(\d{4}).csv")
+
+
+def _read_df(object_name: str) -> pd.DataFrame:
+    """
+    Given an object name, extract the DataFrame stored in it.
+    """
+    string_io = StringIO()
+    string_io.write(read_string_from_object(object_name))
+    string_io.seek(0)
+    df = pd.read_csv(string_io)
+    string_io.close()
+    return df
+
+
+def get_all_data() -> pd.DataFrame:
+    daily_data = get_daily_data()
+    yearly_data = get_yearly_data()
+    all_data = pd.concat([daily_data, yearly_data])
+    return all_data
+
+
+def _daily_object_names() -> Iterator[str]:
+    # Get all objects and limit down to the names that look like daily data.
+    object_names = list_objects_in_bucket()
+    daily_object_names = (
+        name for name in object_names if DAILY_SUMMARY_NAME_REGEX.match(name)
+    )
+    return daily_object_names
+
+
+def get_daily_data() -> pd.DataFrame:
+    """
+    Get all day-level data merged into a single DataFrame.
+    """
+    dfs = []
+    for name in _daily_object_names():
+        df = _read_df(name)
+        # Daily data doesn't have a Date column; we have to infer it from the name.
+        match = DAILY_SUMMARY_NAME_REGEX.match(name)
+        if match is None:
+            raise ValueError(f"name {name} does not resemble a daily file.")
+        date = match.groups()[0]
+        df["Date"] = date.replace("_", "-")
+        dfs.append(df)
+    # Merge them all into one, since the new Date column will make them distinct.
+    full_df = pd.concat(dfs)
+    # Some of the data may have had an dummy column from its original index values.
+    bad_col = "Unnamed: 0"
+    if bad_col in full_df.columns:
+        full_df = full_df.drop(bad_col, axis=1)
+    # Convert the date column into a Pandas date.
+    full_df["Date"] = pd.to_datetime(full_df["Date"])
+    return full_df
+
+
+def _yearly_object_names() -> Iterator[str]:
+    object_names = list_objects_in_bucket()
+    yearly_object_names = (
+        name for name in object_names if YEARLY_SUMMARY_NAME_REGEX.match(name)
+    )
+    return yearly_object_names
+
+
+def get_yearly_data() -> pd.DataFrame:
+    """
+    Get all year-level data merged into a single DataFrame.
+    """
+    # Get all objects and limit down to the names that look like daily data.
+    # Pull out a DataFrame in each object.
+    dfs = (_read_df(name) for name in _yearly_object_names())
+    # No need to add a date column like we do for daily data; yearly data already has
+    # one. We can just merge them together as they are.
+    full_df = pd.concat(dfs)
+    # Convert the date column into a Pandas date.
+    full_df["Date"] = pd.to_datetime(full_df["Date"])
+    return full_df
